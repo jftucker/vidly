f1a6a56e91f8d06ec4954ec9898df6ccf6f43acd
John Tucker <johntucker0686@gmail.com>
2020-07-20 16:45:27 -0700

    uploading coursework

---
+++ src/components/common/like.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Like = ({ onClick, liked }) => {
+  let classes = "fa fa-heart";
+  if (!liked) classes += "-o";
+  return (
+    <i
+      onClick={onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    />
+  );
+};
+
+export default Like;
